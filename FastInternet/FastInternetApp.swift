01043ed9242e2c19dde3df546b298d35664a83f5
Pedro Omar <pocl9812@gmail.com>
2025-12-16 11:51:49 -0500

    Initial Commit

---
+++ FastInternet/FastInternetApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastInternetApp.swift
+//  FastInternet
+//
+//  Created by Pedro Omar  on 12/16/25.
+//
+
+import SwiftUI
+
+@main
+struct FastInternetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
